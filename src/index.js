1c412949410e0e5e9ede4888fd7423bf9f008603
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-05-03 16:21:04 +0530

    Abi Mangalan

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const mongoose = require('mongoose');
